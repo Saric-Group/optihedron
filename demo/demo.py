c914faed1834590b1b3a3d5de3b6720f972e753a
Eugen Rožić <erozic@zoho.eu>
2018-03-07 17:59:46 +0000

    moved "demo" to it's own folder

---
+++ demo/demo.py
@@ -0,0 +1,3 @@
+# import nanoparticle and membranesimulation to construct demo simulation files (script and data) from templates
+
+# maybe add PyLammps code that runs the demo without using the script, to show the alternative...
